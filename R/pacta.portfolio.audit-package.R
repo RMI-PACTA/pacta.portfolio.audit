0d01ecaa2be4b7dd3a09058e52dbc6bbc58641c6
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-05-03 12:31:59 +0200

    export necessary functions

---
+++ R/pacta.portfolio.audit-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr as_tibble
+#' @importFrom dplyr filter
+#' @importFrom dplyr if_else
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom pacta.portfolio.utils data_check
+## usethis namespace: end
+NULL
+
+globalVariables(c(".", ".data"))
