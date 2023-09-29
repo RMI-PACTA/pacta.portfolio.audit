d7971fe48bba76e0862aed8648b5a912aef6045a
CJ Yetman <cj@cjyetman.com>
2023-09-29 13:49:37 +0200

    add `dir` argument to explicitly pass the path while defaulting to old behavior using a global var

--- R/get_entity_info.R
+++ R/get_entity_info.R
@@ -6,13 +6,13 @@
 #'
 #' @export
 
-get_entity_info <- function() {
-  sqlite_path <- file.path(.GlobalEnv$analysis_inputs_path, "entity_info.sqlite")
+get_entity_info <- function(dir = .GlobalEnv$analysis_inputs_path) {
+  sqlite_path <- file.path(dir, "entity_info.sqlite")
   if (file.exists(sqlite_path)) {
     class(sqlite_path) <- c("db_path", "character")
     return(sqlite_path)
   } else {
-    entity_info <- readr::read_rds(file.path(.GlobalEnv$analysis_inputs_path, "entity_info.rds"))
+    entity_info <- readr::read_rds(file.path(dir, "entity_info.rds"))
     return(entity_info)
   }
 }
