31a1232a7593ac0280a68a451fa26045377b644f
CJ Yetman <cj@cjyetman.com>
2024-04-29 11:19:37 +0200

    add tests (#41)

---
+++ tests/testthat/test-portfolio_summary.R
@@ -0,0 +1,20 @@
+test_that("basic functionality works as expected", {
+  portfolio_total <- data.frame(
+    asset_type = c("Equity", "Bonds", "Other"),
+    financial_sector = c("Power", "Oil&Gas", "Automotive"),
+    value_usd = 1:3,
+    valid_input = TRUE
+  )
+
+  expected_output <- data.frame(
+    asset_type = c("Equity", "Bonds", "Other"),
+    financial_sector = c("Power", "Oil&Gas", "Automotive"),
+    valid_input = TRUE,
+    valid_value_usd = 1:3,
+    asset_value_usd = 1:3,
+    valid_value_usd = 6
+  )
+
+  output <- portfolio_summary(portfolio_total)
+  expect_equal(output, expected_output, ignore_attr = TRUE)
+})
